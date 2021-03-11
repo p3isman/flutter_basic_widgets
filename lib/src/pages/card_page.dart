8b77063888c19a213e1bc369fe0dce7f481ea653
P Eisman <peisman@protonmail.com>
2021-03-11 16:03:59 +0100

    update CardPage

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -8,12 +8,58 @@ class CardPage extends StatelessWidget {
         title: Text('Cards'),
       ),
       body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
         children: <Widget>[
           _cardType1(),
+          SizedBox(height: 30),
+          _cardType2(),
         ],
       ),
     );
   }
 
-  Widget _cardType1() {}
+  Widget _cardType1() {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blue,
+            ),
+            title: Text('Card 1'),
+            subtitle: Text('Example text'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(onPressed: () {}, child: Text('Cancel')),
+              TextButton(onPressed: () {}, child: Text('Ok')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardType2() {
+    return Card(
+      child: Column(
+        children: [
+          // Show temporary image while it is loading
+          FadeInImage(
+            placeholder: AssetImage('assets/spinner.gif'),
+            image: NetworkImage(
+                'https://iso.500px.com/wp-content/uploads/2015/11/photo-129299193-1500x775.jpg'),
+            height: 200,
+            fadeInDuration: Duration(milliseconds: 200),
+            fit: BoxFit.cover,
+          ),
+          Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text('This is some example text')),
+        ],
+      ),
+    );
+  }
 }
